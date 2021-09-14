f930e938d661561fb4f4d3dd6c366181bbad3144
Enomine-ndrt <luis.w0587@gmail.com>
2021-09-14 00:59:18 -0500

    api rest mysql golang

---
+++ database/database.go
@@ -0,0 +1,13 @@
+package database
+
+import "database/sql"
+
+func InitDB() *sql.DB {
+	connectionString := "root:@tcp(localhost:3306)/northwind"
+	databaseConnection, err := sql.Open("mysql", connectionString)
+
+	if err != nil {
+		panic(err.Error()) //manejo de errores
+	}
+	return databaseConnection
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module REST_API_MYSQL
+
+go 1.16
+
+require (
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/go-chi/chi/v5 v5.0.3 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+)
